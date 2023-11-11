92b9e1af0a7943aa6ff8e73662766aeacd832855
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-11-11 22:45:28 +0000

    Removes cube from bowling

--- src/main/scala/Bowling.scala
+++ src/main/scala/Bowling.scala
@@ -12,9 +12,6 @@ case class LastFrame(scores: List[Int]) extends BowlingFrame {
 }
 
 object Bowling {
-  def cube(x: Int) =
-    x * x * x
-
   def frames(individualScores: List[String]): List[BowlingFrame] = ???
 
   def scores(frames: List[BowlingFrame]): List[Option[Int]] = ???
