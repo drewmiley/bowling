c483e8201adf5d20cccb6e536a400fed495a41f3
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-11-11 22:34:24 +0000

    Adds implementation of bowling frames tests

--- src/main/scala/Bowling.scala
+++ src/main/scala/Bowling.scala
@@ -3,9 +3,13 @@ trait BowlingFrame
 case class NotLastFrame(scores: List[Int]) extends BowlingFrame {
   def isStrike: Boolean = ???
   def isSpare: Boolean = ???
+
+  def isComplete: Boolean = ???
 }
 
-case class LastFrame(scores: List[Int]) extends BowlingFrame
+case class LastFrame(scores: List[Int]) extends BowlingFrame {
+  def isComplete: Boolean = ???
+}
 
 object Bowling {
   def cube(x: Int) =
--- src/test/scala/BowlingTest.scala
+++ src/test/scala/BowlingTest.scala
@@ -1,68 +1,457 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class BowlingTest extends AnyFunSuite {
-  test("Bowling.frames 1 frame") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 1 frame incomplete") {
+    val frames = List("6")
+    val expected = List(
+      NotLastFrame(List(6))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
-  test("Bowling.frames 2 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 1 frame complete") {
+    val frames = List("6", "2")
+    val expected = List(
+      NotLastFrame(List(6, 2))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
-  test("Bowling.frames 3 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 2 frames incomplete") {
+    val frames = List("6", "2", "7")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
-  test("Bowling.frames 4 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 2 frames complete") {
+    val frames = List("6", "2", "7", "2")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
-  test("Bowling.frames 5 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 3 frames incomplete") {
+    val frames = List("6", "2", "7", "2", "3")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
-  test("Bowling.frames 6 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 3 frames complete") {
+    val frames = List("6", "2", "7", "2", "3", "4")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
-  test("Bowling.frames 7 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 4 frames incomplete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
-  test("Bowling.frames 8 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 4 frames complete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
-  test("Bowling.frames 9 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 5 frames incomplete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
-  test ("Bowling.frames 10 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.frames 5 frames complete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0))
+    )
+    assert(Bowling.frames(frames) == expected)
+  }
+  test("Bowling.frames 6 frames complete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "10")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10))
+    )
+    assert(Bowling.frames(frames) == expected)
+  }
+  test("Bowling.frames 7 frames complete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "10", "10")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10))
+    )
+    assert(Bowling.frames(frames) == expected)
+  }
+  test("Bowling.frames 8 frames complete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "10", "10", "10")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10))
+    )
+    assert(Bowling.frames(frames) == expected)
+  }
+  test("Bowling.frames 9 frames incomplete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "10", "10", "10", "3")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3))
+    )
+    assert(Bowling.frames(frames) == expected)
+  }
+  test("Bowling.frames 9 frames complete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "10", "10", "10", "3", "4")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3, 4))
+    )
+    assert(Bowling.frames(frames) == expected)
+  }
+  test("Bowling.frames 10 frames incomplete 1") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "10", "10", "10", "3", "4", "4")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(4))
+    )
+    assert(Bowling.frames(frames) == expected)
+  }
+  test("Bowling.frames 10 frames incomplete 2") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "10", "10", "10", "3", "4", "4", "6")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(4, 6))
+    )
+    assert(Bowling.frames(frames) == expected)
+  }
+  test("Bowling.frames 10 frames complete") {
+    val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "10", "10", "10", "3", "4", "4", "6", "10")
+    val expected = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(4, 6, 10))
+    )
+    assert(Bowling.frames(frames) == expected)
   }
 
-  test("Bowling.scores 1 frame") {
-    assert(Bowling.cube(3) === 27)
+  //[6,2,7,2,3,4,8,2,9,0,10,10,10,6,3,4,6,10]
+  // [8,17,24,43,52,82,108,127,136,156]
+  test("Bowling.scores 1 frame incomplete") {
+    val scores = List(
+      NotLastFrame(List(6))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
-  test("Bowling.scores 2 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.scores 1 frame complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
-  test("Bowling.scores 3 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.scores 2 scores incomplete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
-  test("Bowling.scores 4 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.scores 2 scores complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
-  test("Bowling.scores 5 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.scores 3 scores incomplete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
-  test("Bowling.scores 6 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.scores 3 scores complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
-  test("Bowling.scores 7 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.scores 4 scores incomplete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
-  test("Bowling.scores 8 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.scores 4 scores complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
-  test("Bowling.scores 9 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.scores 5 scores incomplete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
-  test("Bowling.scores 10 frames") {
-    assert(Bowling.cube(3) === 27)
+  test("Bowling.scores 5 scores complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
+  }
+  test("Bowling.scores 6 scores complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
+  }
+  test("Bowling.scores 7 scores complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
+  }
+  test("Bowling.scores 8 scores complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
+  }
+  test("Bowling.scores 9 scores incomplete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
+  }
+  test("Bowling.scores 9 scores complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3, 4))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
+  }
+  test("Bowling.scores 10 scores incomplete 1") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(4))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
+  }
+  test("Bowling.scores 10 scores incomplete 2") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(4, 6))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
+  }
+  test("Bowling.scores 10 scores complete") {
+    val scores = List(
+      NotLastFrame(List(6, 2)),
+      NotLastFrame(List(7, 2)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(8, 2)),
+      NotLastFrame(List(9, 0)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(10)),
+      NotLastFrame(List(3, 4)),
+      NotLastFrame(List(4, 6, 10))
+    )
+    val expected = List(
+
+    )
+    assert(Bowling.scores(scores) == expected)
   }
 }
-
-//[6,2,7,2,3,4,8,2,9,0,10,10,10,6,3,4,6,10]
-// [8,17,24,43,52,82,108,127,136,156]
