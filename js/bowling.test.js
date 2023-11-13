a5a9540f24ce43232583ba77b83f3b61f940d6cd
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-11-13 17:14:29 +0000

    Work migrating tests find and replace

--- js/bowling.test.js
+++ js/bowling.test.js
@@ -12,203 +12,203 @@ test("Bowling.frames 1 frame incomplete", () => {
     expect(Bowling.frames(frames)).toStrictEqual(expected);
 });
 
-  // test("Bowling.frames 1 frame complete") {
-  //   val frames = List("6", "2")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2))
+  // test("Bowling.frames 1 frame complete", () => {
+  //   const frames = ["6", "2")
+  //   const expected = [
+  //     NotLastFrame([6, 2))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 2 frames incomplete") {
-  //   val frames = List("6", "2", "7")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7))
+  // test("Bowling.frames 2 frames incomplete", () => {
+  //   const frames = ["6", "2", "7")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 2 frames complete") {
-  //   val frames = List("6", "2", "7", "2")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2))
+  // test("Bowling.frames 2 frames complete", () => {
+  //   const frames = ["6", "2", "7", "2")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 3 frames incomplete") {
-  //   val frames = List("6", "2", "7", "2", "3")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3))
+  // test("Bowling.frames 3 frames incomplete", () => {
+  //   const frames = ["6", "2", "7", "2", "3")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 3 frames complete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4))
+  // test("Bowling.frames 3 frames complete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 4 frames incomplete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8))
+  // test("Bowling.frames 4 frames incomplete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 4 frames complete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2))
+  // test("Bowling.frames 4 frames complete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 5 frames incomplete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9))
+  // test("Bowling.frames 5 frames incomplete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 5 frames complete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9, 0))
+  // test("Bowling.frames 5 frames complete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9, 0))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 6 frames complete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9, 0)),
-  //     NotLastFrame(List(10))
+  // test("Bowling.frames 6 frames complete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9, 0]),
+  //     NotLastFrame([10))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 7 frames complete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9, 0)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10))
+  // test("Bowling.frames 7 frames complete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9, 0]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 8 frames complete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9, 0)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10))
+  // test("Bowling.frames 8 frames complete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9, 0]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 9 frames incomplete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9, 0)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(6))
+  // test("Bowling.frames 9 frames incomplete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9, 0]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([6))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 9 frames complete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9, 0)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(6, 3))
+  // test("Bowling.frames 9 frames complete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9, 0]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([6, 3))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
   // test("Bowling.frames 10 frames incomplete 1") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9, 0)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(6, 3)),
-  //     LastFrame(List(4))
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9, 0]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([6, 3]),
+  //     LastFrame([4))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
   // test("Bowling.frames 10 frames incomplete 2") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4", "/")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9, 0)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(6, 3)),
-  //     LastFrame(List(4, 6))
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4", "/")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9, 0]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([6, 3]),
+  //     LastFrame([4, 6))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
-  // test("Bowling.frames 10 frames complete") {
-  //   val frames = List("6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4", "/", "10")
-  //   val expected = List(
-  //     NotLastFrame(List(6, 2)),
-  //     NotLastFrame(List(7, 2)),
-  //     NotLastFrame(List(3, 4)),
-  //     NotLastFrame(List(8, 2)),
-  //     NotLastFrame(List(9, 0)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(10)),
-  //     NotLastFrame(List(6, 3)),
-  //     LastFrame(List(4, 6, 10))
+  // test("Bowling.frames 10 frames complete", () => {
+  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4", "/", "10")
+  //   const expected = [
+  //     NotLastFrame([6, 2]),
+  //     NotLastFrame([7, 2]),
+  //     NotLastFrame([3, 4]),
+  //     NotLastFrame([8, 2]),
+  //     NotLastFrame([9, 0]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([10]),
+  //     NotLastFrame([6, 3]),
+  //     LastFrame([4, 6, 10))
   //   )
-  //   assert(Bowling.frames(frames) == expected)
+  //   assert(Bowling.frames(frames)).toStrictEqual(expected)
   // }
