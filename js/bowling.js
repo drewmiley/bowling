ec33ebb9b0d23227fbf6dfcb3e022b85268b4eb4
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-11-13 18:58:43 +0000

    Implements frames unit tests and helper functions

--- js/bowling.js
+++ js/bowling.js
@@ -1,17 +1,39 @@
-function add(numbers) {
-    return numbers
-        .split(',')
-        .map(x => parseInt(x))
-        .reduce((a, b) => a + b)
+const sum = numbers => numbers.reduce((acc, d) => acc + d, 0);
+
+const getNumericalValueFromScoreAndLastFrame = (lastFrame, score) => {
+    if (score == "X") return 10;
+    if (score == "/") return 10 - lastFrame.scores[lastFrame.scores.length - 1];
+    return parseInt(score, 10);
 }
 
-function NotLastFrame(scores) {
+const NotLastFrame = scores => {
+    const isStrike = scores.length == 1 && scores[0] == 10
     return {
-        scores
+        scores,
+        isStrike,
+        isSpare: scores.length == 2 && sum(scores) == 10,
+        isComplete: isStrike || scores.length == 2
     }
-}
+};
+
+const LastFrame = scores => {
+    return {
+        scores,
+        isComplete: (scores.length == 2 && sum(scores) < 10) || scores.length == 3
+    }
+};
 
 function frames(individualScores) {
+    // individualScores.foldLeft(List.empty[BowlingFrame])((acc, d) => {
+    //   val numericalValue: Int = getNumericalValueFromScoreAndLastFrame(acc.lastOption, d)
+    //   (acc.lastOption, acc.length) match {
+    //     case (Some(notLastFrame: NotLastFrame), 9) if notLastFrame.isComplete => acc :+ LastFrame(List(numericalValue))
+    //     case (Some(notLastFrame: NotLastFrame), _) if notLastFrame.isComplete => acc :+ NotLastFrame(List(numericalValue))
+    //     case (Some(notLastFrame: NotLastFrame), _) => acc.init ++ List(NotLastFrame(notLastFrame.scores :+ numericalValue))
+    //     case (Some(lastFrame: LastFrame), 10) => acc.init ++ List(LastFrame(lastFrame.scores :+ numericalValue))
+    //     case (None, 0) => List(NotLastFrame(List(numericalValue)))
+    //   }
+    // })
     return [
         NotLastFrame([6])
     ];
@@ -21,6 +43,6 @@ const Bowling = {
     frames
 };
 
-exports.add = add;
 exports.NotLastFrame = NotLastFrame;
+exports.LastFrame = LastFrame;
 exports.Bowling = Bowling;
--- js/bowling.test.js
+++ js/bowling.test.js
@@ -1,8 +1,4 @@
-const {add, NotLastFrame, Bowling} = require('./bowling')
-
-test('string with a single number should result in the number itself', () => {
-    expect(add('1')).toBe(1);
-  });
+const {NotLastFrame, LastFrame, Bowling} = require('./bowling')
 
 test("Bowling.frames 1 frame incomplete", () => {
     const frames = ["6"];
@@ -15,200 +11,216 @@ test("Bowling.frames 1 frame incomplete", () => {
 test("Bowling.frames 1 frame complete", () => {
     const frames = ["6", "2"]
     const expected = [
-      NotLastFrame([6, 2])
+        NotLastFrame([6, 2])
     ];
     expect(Bowling.frames(frames)).toStrictEqual(expected)
 });
-  // test("Bowling.frames 2 frames incomplete", () => {
-  //   const frames = ["6", "2", "7")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 2 frames complete", () => {
-  //   const frames = ["6", "2", "7", "2")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 3 frames incomplete", () => {
-  //   const frames = ["6", "2", "7", "2", "3")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 3 frames complete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 4 frames incomplete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 4 frames complete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 5 frames incomplete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 5 frames complete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9, 0))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 6 frames complete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9, 0]),
-  //     NotLastFrame([10))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 7 frames complete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9, 0]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 8 frames complete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9, 0]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 9 frames incomplete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9, 0]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([6))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 9 frames complete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9, 0]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([6, 3))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 10 frames incomplete 1") {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9, 0]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([6, 3]),
-  //     LastFrame([4))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 10 frames incomplete 2") {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4", "/")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9, 0]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([6, 3]),
-  //     LastFrame([4, 6))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
-  // test("Bowling.frames 10 frames complete", () => {
-  //   const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4", "/", "10")
-  //   const expected = [
-  //     NotLastFrame([6, 2]),
-  //     NotLastFrame([7, 2]),
-  //     NotLastFrame([3, 4]),
-  //     NotLastFrame([8, 2]),
-  //     NotLastFrame([9, 0]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([10]),
-  //     NotLastFrame([6, 3]),
-  //     LastFrame([4, 6, 10))
-  //   )
-  //   expect(Bowling.frames(frames)).toStrictEqual(expected)
-  // }
+
+test("Bowling.frames 2 frames incomplete", () => {
+    const frames = ["6", "2", "7"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 2 frames complete", () => {
+    const frames = ["6", "2", "7", "2"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 3 frames incomplete", () => {
+    const frames = ["6", "2", "7", "2", "3"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 3 frames complete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 4 frames incomplete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 4 frames complete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 5 frames incomplete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 5 frames complete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9, 0])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 6 frames complete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9, 0]),
+        NotLastFrame([10])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 7 frames complete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9, 0]),
+        NotLastFrame([10]),
+        NotLastFrame([10])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 8 frames complete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9, 0]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([10])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 9 frames incomplete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9, 0]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([6])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 9 frames complete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9, 0]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([6, 3])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 10 frames incomplete 1", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9, 0]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([6, 3]),
+        LastFrame([4])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 10 frames incomplete 2", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4", "/"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9, 0]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([6, 3]),
+        LastFrame([4, 6])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
+
+test("Bowling.frames 10 frames complete", () => {
+    const frames = ["6", "2", "7", "2", "3", "4", "8", "/", "9", "0", "X", "X", "X", "6", "3", "4", "/", "10"]
+    const expected = [
+        NotLastFrame([6, 2]),
+        NotLastFrame([7, 2]),
+        NotLastFrame([3, 4]),
+        NotLastFrame([8, 2]),
+        NotLastFrame([9, 0]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([10]),
+        NotLastFrame([6, 3]),
+        LastFrame([4, 6, 10])
+    ];
+    expect(Bowling.frames(frames)).toStrictEqual(expected)
+});
